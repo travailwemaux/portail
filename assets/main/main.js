ddc9ce95537283a4f6b7a509a48f7927bffbbd40
travailwemaux <102801799+travailwemaux@users.noreply.github.com>
2022-05-14 17:35:01 +0200

    Add files via upload

---
+++ assets/main/main.js
@@ -0,0 +1,15 @@
+var data = {
+    "application" : [
+        ["ActiPrint","Commande de linge","fa-shirt"],
+        ["APM","Site d'informations / actualités","fa-rss"],
+        ["Ariane","Archives des dossiers patient","fa-folders"],
+        ["E-planning","Planning du personnel","fa-calendar"],
+        ["Ext. Tourcoing","Extranet CH-Tourcoing","fa-network-wired"],
+        ["GED","Gestion documentaire","fa-books"],
+        ["HMSTE","Gestion de la stérilisation","fa-hands-bubbles"],
+        ["Hospitalis","Achat et approvisionnement","fa-money-check-dollar"],
+        ["PMSIpilot","Pilotage médico-économique","fa-user-pilot"],
+        ["Roublac","Recherche d'un code CCAM","fa-magnifying-glass"],
+    ],
+    "icons" : "fa-regular"
+}
